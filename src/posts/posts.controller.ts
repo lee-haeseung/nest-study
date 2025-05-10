095b6af05c09813b27fd78918ebf993245a8e66d
lee-haeseung <lhser123@cau.ac.kr>
2025-05-10 23:17:34 +0900

    feat: post service 설정

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,7 +3,6 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Patch,
   Post,
@@ -14,62 +13,21 @@ import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller('posts')
 export class PostsController {
-  postList: PostModel[] = [
-    {
-      id: 1,
-      title: 'First Post',
-      content: 'This is the content of the first post.',
-      authorId: 1,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-    {
-      id: 2,
-      title: 'Second Post',
-      content: 'This is the content of the second post.',
-      authorId: 2,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-    {
-      id: 3,
-      title: 'Third Post',
-      content: 'This is the content of the third post.',
-      authorId: 1,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  ];
-
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
   async findAll(): Promise<PostModel[]> {
-    return this.postList;
+    return await this.postsService.findAll();
   }
 
   @Get(':id')
   async findOne(@Param('id') id: number): Promise<PostModel> {
-    console.log('findOne', typeof id);
-    const post = this.postList.find((post) => post.id === id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-    return post;
+    return await this.postsService.findOne(id);
   }
 
   @Post()
   async createPost(@Body() dto: CreatePostDto): Promise<PostModel> {
-    const newPost: PostModel = {
-      id: (this.postList[this.postList.length - 1]?.id ?? 0) + 1,
-      title: dto.title,
-      content: dto.content,
-      authorId: dto.authorId,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-    this.postList.push(newPost);
-    return newPost;
+    return await this.postsService.createPost(dto);
   }
 
   @Patch(':id')
@@ -77,26 +35,11 @@ export class PostsController {
     @Param('id') id: number,
     @Body() dto: CreatePostDto,
   ): Promise<PostModel> {
-    const postIndex = this.postList.findIndex((post) => post.id === +id);
-    if (postIndex === -1) {
-      throw new NotFoundException();
-    }
-    const updatedPost: PostModel = {
-      ...this.postList[postIndex],
-      ...dto,
-      updatedAt: new Date(),
-    };
-    this.postList[postIndex] = updatedPost;
-    return updatedPost;
+    return await this.postsService.updatePost(id, dto);
   }
 
   @Delete(':id')
   async deletePost(@Param('id') id: number): Promise<string> {
-    const postIndex = this.postList.findIndex((post) => post.id === +id);
-    if (postIndex === -1) {
-      throw new NotFoundException();
-    }
-    this.postList.splice(postIndex, 1);
-    return '삭제 완료';
+    return await this.postsService.deletePost(id);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PostModel } from './entities/post.entity';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  postList: PostModel[] = [
+    {
+      id: 1,
+      title: 'First Post',
+      content: 'This is the content of the first post.',
+      authorId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      title: 'Second Post',
+      content: 'This is the content of the second post.',
+      authorId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 3,
+      title: 'Third Post',
+      content: 'This is the content of the third post.',
+      authorId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ];
+
+  findAll(): PostModel[] {
+    return this.postList;
+  }
+
+  findOne(id: number): PostModel {
+    console.log('findOne', typeof id);
+    const post = this.postList.find((post) => post.id === id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  createPost(dto: CreatePostDto): PostModel {
+    const newPost: PostModel = {
+      id: (this.postList[this.postList.length - 1]?.id ?? 0) + 1,
+      title: dto.title,
+      content: dto.content,
+      authorId: dto.authorId,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    this.postList.push(newPost);
+    return newPost;
+  }
+
+  updatePost(id: number, dto: CreatePostDto): PostModel {
+    const postIndex = this.postList.findIndex((post) => post.id === +id);
+    if (postIndex === -1) {
+      throw new NotFoundException();
+    }
+    const updatedPost: PostModel = {
+      ...this.postList[postIndex],
+      ...dto,
+      updatedAt: new Date(),
+    };
+    this.postList[postIndex] = updatedPost;
+    return updatedPost;
+  }
+
+  deletePost(id: number): string {
+    const postIndex = this.postList.findIndex((post) => post.id === +id);
+    if (postIndex === -1) {
+      throw new NotFoundException();
+    }
+    this.postList.splice(postIndex, 1);
+    return '삭제 완료';
+  }
+}
